9a18c0de56b8b28952e0715c9f6be1c128db5f8d
rizafrivaldi <rizafrivaldi@gmail.com>
2025-11-24 22:35:49 +0700

    update

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -22,9 +22,7 @@ let validRefreshTokens = [];
 //Endpoint Register//
 router.post("/register", async (req, res) => {
   try {
-    const { username, email, password } = req.body; //Ambil Data Dari Body Request//
-
-    //Validasi Input//
+    const { username, email, password } = req.body;
     if (!username || !email || !password) {
       return res.status(400).json({ message: "Semua field harus diisi" });
     }
@@ -52,8 +50,8 @@ router.post("/register", async (req, res) => {
       select: {id: true, username: true, email: true, role: true, createdAt: true, updatedAt: true},
     });
 
-    //Response Berhasil//
-    return res.status(201).json({ message: "Register berhasil", user: newUser });
+    return res.status(201).json({ message: "Register berhasil", user: newUser,
+    });
   } catch (error) {
     res.status(500).json({ message: "Terjadi kesalahan server" });
   }
@@ -63,12 +61,12 @@ router.post("/register", async (req, res) => {
 router.post("/login", async (req, res) => {
   try {
     const { email, password } = req.body;
-    //Validasi Input//
+    
     if (!email || !password) {
       return res
         .status(400)
         .json({ message: "Email dan password wajib diisi!" });
-    }
+    
 
     //Cek Email User Ada Di "Database" Sementara//
     const user = await prisma.user.findUnique({
@@ -76,7 +74,6 @@ router.post("/login", async (req, res) => {
     });
     if (!user) {
       return res.status(404).json({ message: "User tidak ditemukan!" });
-    }
 
     //Cocokan Password Hashed VS Password Input//
     const match = await bcrypt.compare(password, user.password);
@@ -113,18 +110,20 @@ router.post("/login", async (req, res) => {
 });
 
 //Endpoint Refresh Token//
-router.post("/refresh", (req, res) => {
-  const { refreshToken } = req.body; //Ambil refresh token dari body request//
+router.post("/refresh", async (req, res) => {
+  try {
+  const { refreshToken } = req.body;
 
-  //Cek Apakah Token Ada//
   if (!refreshToken)
     return res.status(401).json({ message: "Refresh token tidak ditemukan" });
 
   //Cari Refresh Token Di Database Dan Pastikan Belum Direvoke//
-  const tokenRow = await prisma.refreshToken.findUnique({ where: { token: refreshToken } });
-  if (!tokenRow || tokenRow.revoked) {
+  const tokenRow = await prisma.refreshToken.findUnique({ where: { token: refreshToken },
+  });
+
+  if (!tokenRow || tokenRow.revoked) 
     return res.status(403).json({ message: "Refresh token tidak valid" });
-  }
+
 
   {/*
   //Cek Apakah Refresh Token Ada Di Daftar Token Valid (Whitelist)//
@@ -137,18 +136,17 @@ router.post("/refresh", (req, res) => {
   */}
 
   //Verifikasi Refresh Token Dengan JWT//
-  jwt.verify(refreshToken, process.env.JWT_REFRESH_SECRET, (err, decoded) => {
+  jwt.verify(refreshToken, process.env.JWT_REFRESH_SECRET, async(err, decoded) => {
     if (err)
       return res.status(403).json({ message: "Refresh token kedaluwarsa" });
 
     //Decoded Hanya Berisi id (Sesuai Payload Saat Membuat Refresh Token) - Cari User Berdasarkan decoded.id//
-    const foundUser = await prisma.user.findUnique({
-      where: { id: decoded.id }
+    const user = await prisma.user.findUnique({
+      where: { id: decoded.id },
     });
 
-    if (!foundUser) {
+    if (!user) 
       return res.status(404).json({ message: "User tidak ditemukan" });
-    }
 
     //Buat Access Token Baru//
     const newAccessToken = generateAccessToken(user);
@@ -158,16 +156,20 @@ router.post("/refresh", (req, res) => {
       accessToken: newAccessToken,
     });
   });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Terjadi kesalahan server" });
+  }
 });
 
 //Endpoint Logout//
-router.post("/logout", (req, res) => {
+router.post("/logout", async (req, res) => {
   const { refreshToken } = req.body;
+
   if (!refreshToken) {
     return res
       .status(400)
       .json({ success: false, message: "Refresh token tidak ditemukan" });
-  }
 
   {/*
   //Cek Apakah Token Ada Di Daftar Valid Refresh Tokens//
@@ -190,29 +192,48 @@ router.post("/logout", (req, res) => {
 
   */}
 
-  await prisma.refreshToken.update({
-    where: { token: tokenRow},
-    data: { revoked: true },
+  const tokenRow = await prisma.refreshToken.findUnique({
+    where: { token: refreshToken},
   });
+
+  if (!tokenRow) {
+    return res.status(403).json({
+      message: "Refresh token tidak valid",
+    });
+  
+    await prisma.refreshToken.update({
+      where: { token: refreshToken },
+      data: { revoked: true },
+    });
+  }
 
   //Response Berhasil Logout//
   return res.json({
     success: true,
     message: "Logout berhasil, token dihapus dari daftar aktif",
   });
+} catch (error) {
+  console.error(error);
+  res.status(500).json({ message: "Terjadi kesalahan server" });
+}
 });
 
 //Test Protected Route//
-router.get("/profile", protect, (req, res) => {
+router.get("/profile", protect, async (req, res) => {
+  try {
   const user = await prisma.user.findUnique({
     where: { id: req.user.id },
-    select: {id: true, username: true, email: true, role: true, createdAt: true, updatedAt: true},
+    select: {id: true, username: true, email: true, role: true, createdAt: true, updatedAt: true,},
   });
 
-  res.json({
+  return res.json({
     message: "Berhasil mengakses route yang dilindungi",
     user
   });
+} catch (error) {
+  console.error(error);
+  res.status(500).json({ message: "Terjadi kesalahan server" });
+}
 });
 
 //Export Router//
